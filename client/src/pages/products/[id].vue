13c3f33c26390f5494bc707ca90e2f81776dd9c6
BryanShea0207 <Bryanshea0207@gmail.com>
2025-03-12 12:47:12 -0400

    updated id.vue

--- client/src/pages/products/[id].vue
+++ client/src/pages/products/[id].vue
@@ -2,13 +2,13 @@
 import { getOne } from '@/models/products'
 import { useRoute } from 'vue-router'
 
-const route = useRoute('/products/[id]')
-const product = getOne(route.params.id)
+/*const route = useRoute('/products/[id]')
+const product = getOne(route.params.id)*/
 </script>
 
 <template>
   <div>
-    <h1 class="title">{{ product.title }} }}</h1>
+    <h1 class="title">{{}}</h1>
     <dev class="product">
       <div class="product-info"></div>
     </dev>
