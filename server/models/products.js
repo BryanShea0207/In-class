06c4de355215e48585d101f441f00cd42f91bca9
BryanShea0207 <Bryanshea0207@gmail.com>
2025-03-27 15:09:03 -0400

    filled in the products model

--- server/models/products.js
+++ server/models/products.js
@@ -2,23 +2,38 @@
 const data = require("../data/products.json")
 
 function getAll(){
-    
+    return data
 }
 
 function get(id) {
-
+    return data.find((item) => item.id == id)
 }
 
-function create(){
-
+function create(item){
+    const newItem = {
+        id: data.length + 1,
+        ...item
+    }
+    data.push(newItem)
+    return newItem
 }
 
-function update(id, product){
-
+function update(id, item){
+    const index = data.findIndex((item) => item.id == id)
+    if (index === -1){
+        return null
+    }
+    const updateItem = {
+        ...data[index],
+        ...item
+    }
+    data[index] = updateItem
+    return updatedItem
 }
 
 function remove(id){
-
+    const index = data.findIndex((item) => item.id == id)
+    data.items.splice(index)
 }
 
 module.exports = {
