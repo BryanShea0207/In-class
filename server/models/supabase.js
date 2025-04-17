0d81ad264190d7289147807912a7306644ff9bb3
BryanShea0207 <Bryanshea0207@gmail.com>
2025-04-17 14:35:13 -0400

    added database implementation

--- server/models/supabase.js
+++ server/models/supabase.js
@@ -1,9 +1,5 @@
 const { createClient } = require( '@supabase/supabase-js')
 
-const supabaseUrl = 'https://spcjgfclstywswkguurz.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-//const supabase = createClient(supabaseUrl, supabaseKey)
-
 module.exports = {
     connect(){
         return createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SECRET_KEY)
